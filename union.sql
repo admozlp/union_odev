9aa42b0deeb0dad82049b31e29a436747b428121
admozlp <efepatnos07@gmail.com>
2022-12-04 17:53:49 +0300

    Tamamlandi

---
+++ union.sql
@@ -0,0 +1,64 @@
+/*
+(
+	select first_name from actor
+)
+union
+(
+	select first_name from customer
+);
+
+(
+	select first_name from actor
+)
+INTERSECT
+(
+	select first_name from customer
+);
+
+
+(
+	select first_name from actor
+)
+EXCEPT
+(
+	select first_name from customer
+);
+*/
+
+
+/*
+(
+	select first_name from actor
+)
+union all
+(
+	select first_name from customer
+);
+
+(
+	select first_name from actor
+)
+INTERSECT all
+(
+	select first_name from customer
+);
+
+
+(
+	select first_name from actor
+)
+EXCEPT all
+(
+	select first_name from customer
+);
+
+*/
+
+
+
+
+
+
+
+
+
